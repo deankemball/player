6b8dec657b8664776818f50d6f1f96365f38581d
inpatr <i.traub@posteo.de>
2022-03-15 19:56:54 +0100

    play button should play something

--- src/player.ts
+++ src/player.ts
@@ -2,10 +2,10 @@ window.addEventListener("load", () => {
   const audioFile = "assets/01_Low_Fi.mp3";
   // var audio = new Audio(audioFile);
   const audio = document.getElementById("audio") as HTMLAudioElement;
-
   const audioSource = document.getElementById(
     "audioSource"
   ) as HTMLAudioElement;
+
   audioSource.src = audioFile;
   var PlayButton = document.getElementById("play") as HTMLElement;
   var SkipBack = document.getElementById("skip-back") as HTMLElement;
@@ -14,7 +14,16 @@ window.addEventListener("load", () => {
   // Make Playbutton work
 
   PlayButton.addEventListener("click", () => {
+    let audioSource = document.getElementById(
+      "audioSource"
+    ) as HTMLAudioElement;
+    audio.removeChild(audioSource);
+    audio.load();
+    //@ts-ignore
+    audioSource = document.createElement("source");
     audioSource.src = audioFile;
+    audioSource.id = "audioSource";
+    audio.appendChild(audioSource);
     console.log("play");
     audio.play();
   });
