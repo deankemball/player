6d262b89bc95281a5fde51e818f14193d3afec2b
Dean Wallflower <dean@Deans-MacBook-Pro.local>
2022-03-15 16:11:27 +0100

    custom styles added to tw.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 module.exports = {
   mode: 'jit',
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'bg-color': '#26292C',
+      'text-color': '#EDF5FC',
+      'accent-one': '#A48DFF',
+      'accent-two': '#AEE2CD',
+    },
+    fontFamily: {
+      'ibm': ['IBM Plex Sans', 'regular', 'medium', 'semibold', 'bold']
+    },
+    extend: {
+      boxShadow: {
+        'md': '0 0.1rem 0.2rem rgba(0, 0, 0, 0.24), 0 0.2rem 0.4rem rgba(0, 0, 0, 0.2), 0 0.1rem 0.8rem rgba(0, 0, 0, 0.18)',
+        'inner': 'inset 0.4rem 0.4rem 1rem 0 rgba(255, 0, 0, 0.25), inset -0.4rem -0.4rem 1rem 0 rgba(255, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
