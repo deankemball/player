9254edde935fd8d5ecd01fa570292e6e7c7a135c
inpatr <i.traub@posteo.de>
2022-03-15 19:07:55 +0100

    nah kinda working

--- src/player.ts
+++ src/player.ts
@@ -1,14 +1,23 @@
 window.addEventListener("load", () => {
-  var audio = new Audio("01_Low_Fi.mp3");
+  const audioFile = "assets/01_Low_Fi.mp3";
+  // var audio = new Audio(audioFile);
+  const audio = document.getElementById("audio") as HTMLAudioElement;
+
+  const audioSource = document.getElementById(
+    "audioSource"
+  ) as HTMLAudioElement;
+  audioSource.src = audioFile;
   var PlayButton = document.getElementById("play") as HTMLElement;
   var SkipBack = document.getElementById("skip-back") as HTMLElement;
   var SkipForward = document.getElementById("skip-forward") as HTMLElement;
 
   // Make Playbutton work
 
-  PlayButton.onclick = () => {
+  PlayButton.addEventListener("click", () => {
+    audioSource.src = audioFile;
+    console.log("play");
     audio.play();
-  };
+  });
 
   // PlayButton.onclick = () => {
   //   if (audio.paused) {
