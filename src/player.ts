0601567211bf5cd5e3d7c7472ca093b5eb633664
inpatr <i.traub@posteo.de>
2022-03-16 15:40:59 +0100

    dan says looks good

--- src/player.ts
+++ src/player.ts
@@ -1,84 +1,108 @@
 import axios from "axios";
-window.addEventListener("load", async (id) => {
-  let Track;
+import _ from "lodash";
+
+window.addEventListener("load", async () => {
+  let tracks;
   try {
-    Track = await axios.get(`http://localhost:3000/songs/${id}`);
+    tracks = await axios
+      .get(`http://localhost:3000/songs`)
+      .then((res) => res.data);
   } catch (error) {
     console.error(error);
   }
 
+  console.log(tracks);
+  type Track = {
+    id: number;
+    artist: string;
+    album: string;
+    dirPath: string;
+    coverPath: string;
+  };
+
   // access object and get id + path
-  let trackID: number = Track.id;
-  let Path = Track.dirPath;
+  let trackID: number;
+  let Path = tracks.dirPath;
   let nextTrack: number = trackID + 1;
   let prevTrack: number = trackID - 1;
+
+  // grabbing elements
+  const PlayButton = document.getElementById("play") as HTMLElement;
+  const SkipBack = document.getElementById("skip-back") as HTMLElement;
+  const SkipForward = document.getElementById("skip-forward") as HTMLElement;
+  const PauseIcon = document.getElementById("pause-icon") as HTMLElement;
+  const PlayIcon = document.getElementById("play-icon") as HTMLElement;
 
   // basics for "starting" a track
   let currentTrack = document.createElement("audio") as HTMLAudioElement;
   let isPlaying = false;
+  let DivClick = document.getElementById(String(trackID)) as HTMLElement; // div you click (like track in tracklist)
+  let DivID: string;
 
-  function loadTrack(trackID: number) {
-    // get ID from addeventlisteners on albums / tracks etc.
-    // take ID and get path
-    currentTrack.src = Path;
-    currentTrack.load();
-    currentTrack.addEventListener("ended", playNext());
-  }
+  // get ID by clicking on track/album
+  DivClick.addEventListener("click", () => {
+    DivID = DivClick.getAttribute("id");
+    getTrack(trackID);
+  });
+  async function getTrack(trackID: number) {
+    let Track: Track;
+    try {
+      trackID = Number(DivID);
+      Track = await axios
+        .get(`http://localhost:3000/songs/${trackID}`)
+        .then((res) => res.data);
+    } catch (error) {
+      console.error(error);
+    }
 
-  // grabbing elements
-  var PlayButton = document.getElementById("play") as HTMLElement;
-  var SkipBack = document.getElementById("skip-back") as HTMLElement;
-  var SkipForward = document.getElementById("skip-forward") as HTMLElement;
-  var PauseIcon = document.getElementById("pause-icon") as HTMLElement;
-  var PlayIcon = document.getElementById("play-icon") as HTMLElement;
+    async function loadTrack(trackID: number) {
+      await getTrack(trackID);
+      currentTrack.src = Track.dirPath;
+      currentTrack.load();
+      currentTrack.addEventListener("ended", playNext);
+    }
 
-  // Playbutton
-  PlayButton.addEventListener("click", () => {
-    if (isPlaying) {
+    // Playbutton
+    PlayButton.addEventListener("click", () => {
+      if (isPlaying) {
+        currentTrack.play();
+        isPlaying = true;
+        PlayIcon.classList.toggle("hidden");
+        PauseIcon.classList.remove("hidden");
+      } else {
+        currentTrack.pause();
+        isPlaying = false;
+        PlayIcon.classList.remove("hidden");
+        PauseIcon.classList.toggle("hidden");
+      }
+    });
+
+    // Next track
+    function playNext() {
+      if (trackID < 43) {
+        trackID = nextTrack;
+      } else {
+        trackID = 1;
+      }
+      loadTrack(trackID);
       currentTrack.play();
-      isPlaying = true;
-      PlayIcon.classList.toggle("hidden");
-      PauseIcon.classList.remove("hidden");
-    } else {
-      currentTrack.pause();
-      isPlaying = false;
-      PlayIcon.classList.remove("hidden");
-      PauseIcon.classList.toggle("hidden");
     }
-  });
 
-  // Next track
-  function playNext() {
-    if (trackID < 43) {
-      trackID = nextTrack;
-    } else {
-      trackID = 1;
+    // Prev track
+    function playPrev() {
+      trackID = prevTrack;
+      loadTrack(trackID);
+      currentTrack.play();
     }
-    loadTrack(trackID);
-    Track.play();
+
+    // Skip Forward
+    SkipForward.addEventListener("click", () => {
+      playNext();
+    });
+
+    // Skip Back
+    SkipBack.addEventListener("click", () => {
+      playPrev();
+    });
   }
-
-  // Prev track
-  function playPrev() {
-    trackID = prevTrack;
-    loadTrack(trackID);
-    Track.play();
-  }
-
-  // Skip Forward
-  SkipForward.addEventListener("click", () => {
-    playNext();
-  });
-
-  // Skip Back
-  SkipBack.addEventListener("click", () => {
-    playPrev();
-  });
-
-  // grab the time and stick it to the width of new div
-  // var SeekBar = document.getElementById("SeekBar") as HTMLElement;
-  // function updateSeekBar() {
-  //   let percentage = Math.floor((100 / audio.duration) * audio.currentTime);
-  //   Seekbar.classList.toggle("w");
-  // }
 });
